88114a1b38b2433addcc3477ccd35f69cbf7849d
Amee6 <Barotovamin70@gmail.com>
2025-08-05 22:20:18 +0500

    обновил функции

--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
-module github.com/Amee6/stats
+module github.com/Amee6/stats/v2
 
 go 1.24.5
 
-require github.com/Amee6/bank v1.0.0 // indirect
+require (
+	github.com/Amee6/bank v1.0.0 // indirect
+	github.com/Amee6/bank/v2 v2.0.0 // indirect
+	github.com/Amee6/bank/v3 v3.0.0 // indirect
+)
--- pkg/bank/stats/stats.go
+++ pkg/bank/stats/stats.go
@@ -1,12 +1,17 @@
 package stats
 
-import "github.com/Amee6/bank/pkg/types"
+import (
+	
+	"github.com/Amee6/bank/v3/pkg/types"
+)
 
 func Avg(payments [] types.Payment) types.Money	{
 	var avgSum types.Money
-
+	
 	for _, payment := range payments {
+		if payment.Status != " FAIL" {
 		avgSum += payment.Amount
+	}
 	}
 	avgSum = avgSum / types.Money(len(payments))
 	return avgSum
@@ -17,7 +22,7 @@ func TotalICategory(payments [] types.Payment, category types.Category) types.Mo
 	var sum types.Money
 
 	for _, payment := range payments {
-		if payment.Category == category{
+		if payment.Category == category && payment.Status != "FAIL" {
 			sum += payment.Amount
 		}
 	}
