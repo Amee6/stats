5a78e9e8c48a1040446f1724e3adbc14535374db
Amee6 <Barotovamin70@gmail.com>
2025-08-07 16:49:39 +0500

    добавил функцию FilterByCategory

--- pkg/bank/stats/stats.go
+++ pkg/bank/stats/stats.go
@@ -1,24 +1,23 @@
 package stats
 
 import (
-	
 	"github.com/Amee6/bank/v3/pkg/types"
 )
 
-func Avg(payments [] types.Payment) types.Money	{
+func Avg(payments []types.Payment) types.Money {
 	var avgSum types.Money
-	
+
 	for _, payment := range payments {
 		if payment.Status != " FAIL" {
-		avgSum += payment.Amount
-	}
+			avgSum += payment.Amount
+		}
 	}
 	avgSum = avgSum / types.Money(len(payments))
 	return avgSum
 }
 
-func TotalICategory(payments [] types.Payment, category types.Category) types.Money {
-	
+func TotalICategory(payments []types.Payment, category types.Category) types.Money {
+
 	var sum types.Money
 
 	for _, payment := range payments {
@@ -27,4 +26,17 @@ func TotalICategory(payments [] types.Payment, category types.Category) types.Mo
 		}
 	}
 	return sum
+}
+
+// FilterByCategory возвращает платежи в указанной категории
+func FilterByCategory(payments []types.Payment, category types.Category) []types.Payment {
+
+	var filtered []types.Payment
+
+	for _, payment := range payments {
+		if payment.Category == category {
+			filtered = append(filtered, payment)
+		}
+	}
+	return filtered
 }
