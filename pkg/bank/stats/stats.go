a31ff3c5f3271dab679cbe08e99fd176c8cf1282
Amee6 <Barotovamin70@gmail.com>
2025-08-07 16:57:56 +0500

    Добавлена функция TotalInCategory

--- pkg/bank/stats/stats.go
+++ pkg/bank/stats/stats.go
@@ -40,3 +40,14 @@ func FilterByCategory(payments []types.Payment, category types.Category) []types
 	}
 	return filtered
 }
+
+// TotalInCategory возвращает сумму платежей по каждой категории
+func TotalInCategories(payments [] types.Payment) map [types.Category]types.Money {
+	
+	categories := map [types.Category]types.Money{}
+
+	for _, payment := range payments {
+		categories[payment.Category] += payment.Amount
+	}
+	return  categories	
+}
